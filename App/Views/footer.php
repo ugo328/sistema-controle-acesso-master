664800d0e335e0cafd3f88d552277916bbf57ba5
ugo328 <ugorventura@gmail.com>
2024-03-04 05:50:43 -0300

    colocando foto e assinatura

---
+++ App/Views/footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="footer">
+        <div class="copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>© <?php echo date("Y"); ?> All Rights Reserved. Design by Ugo Ventura. 
+                            <a href="https://www.linkedin.com/in/ugo-rocha-ventura-97335a68/" target="_blank">
+                                <i class="fab fa-linkedin"></i> LinkedIn
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
